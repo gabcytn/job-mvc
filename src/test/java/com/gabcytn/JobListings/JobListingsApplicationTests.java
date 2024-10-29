b64045bcb300ae5db4833271b1e16b5c39342e29
gabcytn <cayetanogabriel03@gmail.com>
2024-10-29 21:49:22 +0800

    Initial commit

---
+++ src/test/java/com/gabcytn/JobListings/JobListingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabcytn.JobListings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobListingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
