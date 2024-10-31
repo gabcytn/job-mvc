1dc04ab3270ed09c8f65685b27905e7cf4ae6018
gabcytn <cayetanogabriel03@gmail.com>
2024-10-31 15:58:14 +0800

    Call job repository to get all job listings

--- src/main/java/com/gabcytn/JobListings/Service/JobService.java
+++ src/main/java/com/gabcytn/JobListings/Service/JobService.java
@@ -1,4 +1,25 @@
 package com.gabcytn.JobListings.Service;
 
+import com.gabcytn.JobListings.Model.Job;
+import com.gabcytn.JobListings.Repository.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Lazy
 public class JobService {
+
+    @Autowired
+    JobRepository jobRepository;
+
+    public void addJob(Job job) {
+        jobRepository.save(job);
+    }
+
+    public List<Job> getAllJobs () {
+        return jobRepository.findAll();
+    }
 }
