12fb65ea72da3f3bd8ab14f366593314d0030b5c
gabcytn <cayetanogabriel03@gmail.com>
2024-10-31 15:05:10 +0800

    Add POST mapping to /add-job

--- src/main/java/com/gabcytn/JobListings/Controller/JobController.java
+++ src/main/java/com/gabcytn/JobListings/Controller/JobController.java
@@ -1,7 +1,16 @@
 package com.gabcytn.JobListings.Controller;
 
+import com.gabcytn.JobListings.Model.Job;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+
+import java.util.Map;
 
 @Controller
 public class JobController {
@@ -18,5 +27,13 @@ public class JobController {
     @GetMapping("/view-jobs")
     public String getViewJobs (){
         return "view-all";
+    }
+
+    @PostMapping("/add-job")
+    public ModelAndView postAddJob (Job job, ModelAndView modelAndView) {
+        modelAndView.addObject("job", job);
+        modelAndView.setViewName("post-job");
+
+        return modelAndView;
     }
 }
